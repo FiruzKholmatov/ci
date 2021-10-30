d7c3e5b459cd73996a1197d7f25772deec9b1265
firuz kholmatov <firuzkholmatov@yandex.ru>
2021-10-30 21:01:32 +0300

    achieved 100% coverage

--- src/main/java/ru/netology/stats/StatisticsService.java
+++ src/main/java/ru/netology/stats/StatisticsService.java
@@ -1,19 +1,22 @@
 package ru.netology.stats;
 
 public class StatisticsService {
-    /**
-     * Calculate index of max income
-     *
-     * @param incomes - array of incomes
-     * @return - index of first max value
-     */
-    public long findMax(long[] incomes) {
-        long current_max_index = 0;
-        long current_max = incomes[0];
+
+    public long findSum(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+
+        }
+        return sum;
+    }
+
+    public long findMin(long[] incomes) {
+        long current_min = incomes[0];
         for (long income : incomes)
-            if (current_max < income)
-                current_max = income;
-        return current_max;
+            if (current_min > income)
+                current_min = income;
+        return current_min;
     }
 
 }
